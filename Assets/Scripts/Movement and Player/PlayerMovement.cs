0fe969a91a2ac6def7b2177a9ed4cfa12b9e3522
brody-b9801 <brodymb2@illinois.edu>
2026-07-15 23:19:07 -0500

    Separate renderer for own player and camera orbit work
    Recommit of the self/other player renderer, camera orbit, and
    self-material changes (squashed from prior iterations).

--- Assets/Scripts/Movement and Player/PlayerMovement.cs
+++ Assets/Scripts/Movement and Player/PlayerMovement.cs
@@ -409,7 +409,13 @@ public class PlayerMovement : AttributesSync {
             desertSky = RenderSettings.skybox;
             InitializeDimensions();
             SetActiveDimension(desertInfo);
+            GetComponent<MeshRenderer>().enabled = false;
+        } else {
+            foreach (Transform child in transform) {
+                if (child.name == "Renderer") child.gameObject.SetActive(false);
+            }
         }
+
     }
 
     public static bool getAvatarBool(string avatar1) {
@@ -1242,9 +1248,11 @@ public class PlayerMovement : AttributesSync {
             akm.localPosition = akmBaseLocalPos;
             akm.localEulerAngles = akmBaseLocalRot;
         }
-
         if (!dead) {
-            playerCamera.gameObject.transform.position = transform.position + new Vector3(Mathf.Sin(Camera.main.transform.rotation.y), 0.75f, Mathf.Cos(Camera.main.transform.rotation.y)) + landingCameraOffset;
+            //float CameraBodyOffset = .5f * Mathf.Abs(Mathf.Sin(Camera.main.transform.eulerAngles.x * Mathf.Deg2Rad));
+            //GetComponent<MeshRenderer>().sharedMaterial = (CameraBodyOffset < 0.4f) ? selfMaterial : playerMaterial;
+            playerCamera.gameObject.transform.position = transform.position + new Vector3(.5f * Mathf.Sin(Camera.main.transform.eulerAngles.y * Mathf.Deg2Rad), .75f, .5f * Mathf.Cos(Camera.main.transform.eulerAngles.y * Mathf.Deg2Rad)) + landingCameraOffset;
+            playerCamera.gameObject.transform.position = transform.position + new Vector3(0, .75f, 0) + landingCameraOffset;
             if (!Shooting.reloading) {
                 Vector3 shootOffset = new Vector3((-Shaker.yRot - Shaker.zRot) / 500, Shaker.easedRotationChange / 125, -Shaker.easedRotationChange / 250) * shootAnimTune;
                 if (IsValidVector3(shootOffset))
