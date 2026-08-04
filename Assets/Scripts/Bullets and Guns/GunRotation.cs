4fcb2b2b6bcb4c43800a5ea67b05ab9fff3656fc
brody-b9801 <brodymb2@illinois.edu>
2026-08-04 16:36:43 -0500

    Added magazine synchronization for non-Camera gun

--- Assets/Scripts/Bullets and Guns/GunRotation.cs
+++ Assets/Scripts/Bullets and Guns/GunRotation.cs
@@ -8,10 +8,13 @@ public class GunRotation : AttributesSync
     [SerializeField] private Transform gr;
     [SerializeField] private Transform gun;
     private Transform g1;
+    private Transform gm1;
+    [SerializeField] private Transform gunMag;
     [SerializeField] private Alteruna.Avatar avatar;
 
     void Start() {
         g1 = GameObject.Find("CamAKM").transform;
+        gm1 = GameObject.Find("MC.Magazine").transform;
         if (avatar.IsOwner) {
             Transform rendererContainer = transform.GetChild(0);
             for (int i = 0; i < rendererContainer.childCount; i++)
@@ -28,20 +31,20 @@ public class GunRotation : AttributesSync
     void Update()
     {
         if (g1 != null && IsValidVector3(g1.transform.position) && IsValidQuaternion(g1.transform.rotation))
-        {
-            gunPosition(g1.transform.position - new Vector3(0, 0.35f, 0), g1.transform.rotation);
-        }
+            gunPosition(g1.transform.position - new Vector3(0, 0.35f, 0), g1.transform.rotation, false);
+        if (gm1 != null && IsValidVector3(gm1.transform.position) && IsValidQuaternion(gm1.transform.rotation))
+            gunPosition(gm1.transform.position - new Vector3(0, 0.35f, 0), gm1.transform.rotation, true);
     }
 
-
-    private void gunPosition(Vector3 pos, Quaternion rot)
+    private void gunPosition(Vector3 pos, Quaternion rot, bool isMagazine)
     {
-        if (gun != null && IsValidVector3(pos) && IsValidQuaternion(rot))
+        ref Transform thingToPosition = ref isMagazine ? ref gunMag : ref gun;
+        if (thingToPosition != null && IsValidVector3(pos) && IsValidQuaternion(rot))
         {
-            gun.transform.position = pos;
-            gun.transform.rotation = rot;
-            gun.transform.localEulerAngles = gun.transform.localEulerAngles - new Vector3(0, 0, 0); 
-            gun.transform.localPosition = avatar.IsOwner ? new Vector3(0.6f, gun.transform.localPosition.y - 0.05f, gun.transform.localPosition.z - 0.65f) : new Vector3(0.6f, gun.transform.localPosition.y, gun.transform.localPosition.z + 0.1f);
+            thingToPosition.transform.position = pos;
+            thingToPosition.transform.rotation = rot;
+            thingToPosition.transform.localEulerAngles = thingToPosition.transform.localEulerAngles - new Vector3(0, 0, 0); 
+            thingToPosition.transform.localPosition = avatar.IsOwner ? new Vector3(0.6f, thingToPosition.transform.localPosition.y - 0.05f, thingToPosition.transform.localPosition.z - 0.65f) : new Vector3(0.6f, thingToPosition.transform.localPosition.y, thingToPosition.transform.localPosition.z + 0.1f);
         }
     }
     private bool IsValidVector3(Vector3 vector)
