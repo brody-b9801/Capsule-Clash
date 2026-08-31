b7b29063844ac12fab35da4a928d42d0c7d59817
Brody Blask <brodymb2@illinois.edu>
2026-08-31 14:46:27 -0500

    Refactored zip method to work for different path inputs, only zipping server now and planning to upload to R2, buildclient to ship to steam

--- Assets/Editor/PostBuild.cs
+++ Assets/Editor/PostBuild.cs
@@ -20,38 +20,52 @@ public class PostBuild
         buildPlayerOptions.scenes = new[] { "Assets/Scenes/CombatScene.unity" }; //Change when boss scene added
         
         BuildPlayerOptions serverPlayerOptions = new BuildPlayerOptions();
-        string serverPath = "Builds/WindowsServer/Server.exe";        
+        string serverPath = "Builds/LinuxServer/Server.exe";
         serverPlayerOptions.locationPathName = serverPath;
         serverPlayerOptions.scenes = new[] { "Assets/Scenes/CombatScene.unity" };
         serverPlayerOptions.target = BuildTarget.StandaloneLinux64; 
         serverPlayerOptions.subtarget = (int)StandaloneBuildSubtarget.Server; 
 
         bool startTargetLinux = EditorUserBuildSettings.activeBuildTarget == BuildTarget.StandaloneLinux64;
+        
+        if (startTargetLinux) {
+            BuildServer(ref serverPlayerOptions);
+            BuildClient(ref buildPlayerOptions);    
+        } else {
+            BuildClient(ref buildPlayerOptions);
+            BuildServer(ref serverPlayerOptions);      
+        }
+    }
 
-        BuildReport buildResult = BuildPipeline.BuildPlayer(startTargetLinux ? serverPlayerOptions : buildPlayerOptions);
+    public static void BuildClient(ref BuildPlayerOptions buildPlayerOptions)
+    {
+        BuildReport buildResult = BuildPipeline.BuildPlayer(buildPlayerOptions);
         if (buildResult.summary.result == BuildResult.Succeeded)
         {
-            BuildReport serverResult = BuildPipeline.BuildPlayer(startTargetLinux ? buildPlayerOptions : serverPlayerOptions);
-            if (serverResult.summary.result == BuildResult.Succeeded)
-            {
-                Zip();
-            } else {
-                UnityEngine.Debug.LogError("Server build failed: " + serverResult.summary.result);
-            }
+            //Ship to steam
         } else {
             UnityEngine.Debug.LogError("Client build failed: " + buildResult.summary.result);
         }
-
     }
 
-    public static void Zip()
+    public static void BuildServer(ref BuildPlayerOptions serverPlayerOptions)
+    {
+        BuildReport serverResult = BuildPipeline.BuildPlayer(serverPlayerOptions);
+        if (serverResult.summary.result == BuildResult.Succeeded)
+            Zip("Builds/LinuxServer");
+            //Upload to R2
+        else
+            UnityEngine.Debug.LogError("Server build failed: " + serverResult.summary.result);
+    }
+
+    public static void Zip(string path)
     {
         string zip_path = "Builds.zip";
         if (File.Exists(zip_path))
         {
             File.Delete(zip_path);
         }
-        ZipFile.CreateFromDirectory("Builds", zip_path, System.IO.Compression.CompressionLevel.Optimal, true);
+        ZipFile.CreateFromDirectory(path, zip_path, System.IO.Compression.CompressionLevel.Optimal, true);
     }
 }
 #endif
