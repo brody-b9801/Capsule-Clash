d93668e96470057335e9a90b5c02f5cf7953bdc4
Brody Blask <brodymb2@illinois.edu>
2026-08-31 09:10:37 -0500

    Moved from IPostprocessBuildWithReport to a MonoBehaviour helper that creates the server and player build in the same directory, then zips the file

--- Assets/Editor/PostBuild.cs
+++ Assets/Editor/PostBuild.cs
@@ -8,11 +8,36 @@ using System.Diagnostics;
 using System.IO.Compression;
 using System.IO;
 
-public class PostBuild : IPostprocessBuildWithReport
+public class PostBuild : MonoBehaviour
 {
     public int callbackOrder => 0;
+    [MenuItem("Build")]
+    public static void Build()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        string buildPath = "Builds/WindowsServer/Server.exe";        
+        buildPlayerOptions.locationPathName = buildPath;
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows64; 
+        buildPlayerOptions.subtarget = (int)StandaloneBuildSubtarget.Server;    
+        
+        BuildPlayerOptions serverPlayerOptions = new BuildPlayerOptions();
+        string serverPath = "Builds/WindowsServer/Server.exe";        
+        serverPlayerOptions.locationPathName = serverPath;
+        serverPlayerOptions.target = BuildTarget.StandaloneWindows64; 
+        serverPlayerOptions.subtarget = (int)StandaloneBuildSubtarget.Server; 
 
-    public void OnPostprocessBuild(BuildReport report)
+        BuildReport buildResult = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        if (buildResult.summary.result == BuildResult.Succeeded)
+        {
+            BuildReport serverResult = BuildPipeline.BuildPlayer(serverPlayerOptions);
+            if (serverResult.summary.result == BuildResult.Succeeded)
+            {
+                zip();
+            }
+        }
+    }
+
+    public static void zip()
     {
         string zip_path = "Builds.zip";
         if (File.Exists(zip_path))
