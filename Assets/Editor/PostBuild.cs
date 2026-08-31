65289ec7cc3278d86214f69ec779ecfd62f05088
Brody Blask <brodymb2@illinois.edu>
2026-08-31 09:54:31 -0500

    changed server to linux, and ensured building process starts with the active build target to reduce switching needed, reducing time to build

--- Assets/Editor/PostBuild.cs
+++ Assets/Editor/PostBuild.cs
@@ -16,21 +16,22 @@ public class PostBuild
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
         string buildPath = "Builds/WindowsClient/Client.exe";
         buildPlayerOptions.locationPathName = buildPath;
-        buildPlayerOptions.target = BuildTarget.StandaloneWindows64; 
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
+        buildPlayerOptions.scenes = new[] { "Assets/Scenes/CombatScene.unity" }; //Change when boss scene added
         
         BuildPlayerOptions serverPlayerOptions = new BuildPlayerOptions();
         string serverPath = "Builds/WindowsServer/Server.exe";        
         serverPlayerOptions.locationPathName = serverPath;
         serverPlayerOptions.scenes = new[] { "Assets/Scenes/CombatScene.unity" };
-        serverPlayerOptions.target = BuildTarget.StandaloneWindows64; 
+        serverPlayerOptions.target = BuildTarget.StandaloneLinux64; 
         serverPlayerOptions.subtarget = (int)StandaloneBuildSubtarget.Server; 
 
+        bool startTargetLinux = EditorUserBuildSettings.activeBuildTarget == BuildTarget.StandaloneLinux64;
 
-        BuildReport buildResult = BuildPipeline.BuildPlayer(buildPlayerOptions);
-
+        BuildReport buildResult = BuildPipeline.BuildPlayer(startTargetLinux ? serverPlayerOptions : buildPlayerOptions);
         if (buildResult.summary.result == BuildResult.Succeeded)
         {
-            BuildReport serverResult = BuildPipeline.BuildPlayer(serverPlayerOptions);
+            BuildReport serverResult = BuildPipeline.BuildPlayer(startTargetLinux ? buildPlayerOptions : serverPlayerOptions);
             if (serverResult.summary.result == BuildResult.Succeeded)
             {
                 Zip();
