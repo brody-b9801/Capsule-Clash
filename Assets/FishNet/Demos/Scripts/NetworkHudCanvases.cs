0e4d1318ce9057084dd2cbab2d2af4e6569430f2
Brody Blask <brodymb2@illinois.edu>
2026-09-01 22:58:11 -0500

    Network hud progress, credit updated

--- Assets/FishNet/Demos/Scripts/NetworkHudCanvases.cs
+++ Assets/FishNet/Demos/Scripts/NetworkHudCanvases.cs
@@ -84,7 +84,7 @@ namespace FishNet.Example
 
         private void OnGUI()
         {
-#if ENABLE_INPUT_SYSTEM
+
             string GetNextStateText(LocalConnectionState state)
             {
                 if (state == LocalConnectionState.Stopped)
@@ -98,46 +98,10 @@ namespace FishNet.Example
                 else
                     return "Invalid";
             }
-
-            GUILayout.BeginArea(new(4, 110, 256, 9000));
-            Vector2 defaultResolution = new(1920f, 1080f);
-            GUI.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, new(Screen.width / defaultResolution.x, Screen.height / defaultResolution.y, 1));
-
-            GUIStyle style = GUI.skin.GetStyle("button");
-            int originalFontSize = style.fontSize;
-
-            Vector2 buttonSize = new(165f, 42f);
-            style.fontSize = 26;
-            // Server button.
-            if (Application.platform != RuntimePlatform.WebGLPlayer)
-            {
-                if (GUILayout.Button($"{GetNextStateText(_serverState)} Server", GUILayout.Width(buttonSize.x), GUILayout.Height(buttonSize.y)))
-                    OnClick_Server();
-                GUILayout.Space(10f);
-            }
-
-            // Client button.
-            if (GUILayout.Button($"{GetNextStateText(_clientState)} Client", GUILayout.Width(buttonSize.x), GUILayout.Height(buttonSize.y)))
-                OnClick_Client();
-
-            style.fontSize = originalFontSize;
-
-            GUILayout.EndArea();
-#endif
         }
 
         private void Start()
         {
-#if !ENABLE_INPUT_SYSTEM
-            SetEventSystem();
-            BaseInputModule inputModule = FindObjectOfType<BaseInputModule>();
-            if (inputModule == null)
-                gameObject.AddComponent<StandaloneInputModule>();
-#else
-            _serverIndicator.transform.gameObject.SetActive(false);
-            _clientIndicator.transform.gameObject.SetActive(false);
-#endif
-
             _networkManager = FindObjectOfType<NetworkManager>();
             if (_networkManager == null)
             {
@@ -151,11 +115,6 @@ namespace FishNet.Example
                 _networkManager.ServerManager.OnServerConnectionState += ServerManager_OnServerConnectionState;
                 _networkManager.ClientManager.OnClientConnectionState += ClientManager_OnClientConnectionState;
             }
-
-            if (_autoStartType == AutoStartType.Host || _autoStartType == AutoStartType.Server)
-                OnClick_Server();
-            if (!Application.isBatchMode && (_autoStartType == AutoStartType.Host || _autoStartType == AutoStartType.Client))
-                OnClick_Client();
         }
 
         private void OnDestroy()
@@ -207,7 +166,6 @@ namespace FishNet.Example
             else
                 _networkManager.ServerManager.StartConnection();
 
-            DeselectButtons();
         }
 
         public void OnClick_Client()
@@ -220,26 +178,8 @@ namespace FishNet.Example
             else
                 _networkManager.ClientManager.StartConnection();
 
-            DeselectButtons();
         }
 
-        private void SetEventSystem()
-        {
-#if !ENABLE_INPUT_SYSTEM
-            if (_eventSystem != null)
-                return;
-            _eventSystem = FindObjectOfType<EventSystem>();
-            if (_eventSystem == null)
-                _eventSystem = gameObject.AddComponent<EventSystem>();
-#endif
-        }
 
-        private void DeselectButtons()
-        {
-#if !ENABLE_INPUT_SYSTEM
-            SetEventSystem();
-            _eventSystem?.SetSelectedGameObject(null);
-#endif
-        }
     }
 }
