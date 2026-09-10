b360c520d611006f3b02b7d5165fb4292ff36541
Brody Blask <brodymb2@illinois.edu>
2026-09-10 03:10:45 -0500

    moved gun to a network transform, kept mag and casing as server/client rpcs, but made them only sync on position change to reduce bandwidth consumption

--- Assets/Scripts/Bullets and Guns/GunRotation.cs
+++ Assets/Scripts/Bullets and Guns/GunRotation.cs
@@ -1,98 +1,139 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using FishNet.Object;
-using FishNet.Object.Synchronizing;
+using FishNet.Transporting;
 
 public class GunRotation : NetworkBehaviour
 {
     [SerializeField] private Transform gr;
-    [SerializeField] private Transform gun;
-    [SerializeField] private Transform casing;       
+    [SerializeField] private Transform gun;    
+    [SerializeField] private Transform casing;
     [SerializeField] private Transform gunMag;
 
     private Transform g1;
     private Transform gm1;
     private Transform c1;
 
-    Vector3 magPos;
-    Vector3 magPosPrev;
-    Vector3 casingPos;
-    Vector3 casingPosPrev;
+    private MeshRenderer casingRenderer;
+    private MeshRenderer magRenderer;
+    private MeshRenderer c1Renderer;
+    private MeshRenderer gm1Renderer;
 
-    public override void OnStartClient() {
+    private Vector3 casingPosPrev;
+    private Vector3 magPosPrev;
+    private bool casingEnabledPrev;
+    private bool magEnabledPrev;
+    private bool sentOnce;
+
+    public override void OnStartClient()
+    {
         base.OnStartClient();
 
-        if (IsOwner) {
-            g1 = SceneLookup.FindInactive("CamAKM").transform;
-            gm1 = SceneLookup.FindInactive("MC.Magazine").transform;
-            c1 = SceneLookup.FindInactive("CamCasing").transform;
-            Transform rendererContainer = transform.GetChild(0);
-            for (int i = 0; i < rendererContainer.childCount; i++)
+        casingRenderer = casing.GetComponent<MeshRenderer>();
+        magRenderer = gunMag.GetComponent<MeshRenderer>();
+
+        if (!IsOwner) return;
+
+        g1 = SceneLookup.FindInactive("CamAKM").transform;
+        gm1 = SceneLookup.FindInactive("MC.Magazine").transform;
+        c1 = SceneLookup.FindInactive("CamCasing").transform;
+
+        c1Renderer = c1.GetComponent<MeshRenderer>();
+        gm1Renderer = gm1.GetComponent<MeshRenderer>();
+
+        Transform rendererContainer = transform.GetChild(0);
+        for (int i = 0; i < rendererContainer.childCount; i++)
+        {
+            Transform child = rendererContainer.GetChild(i);
+            if (child.TryGetComponent<Renderer>(out Renderer renderer))
             {
-                Transform child = rendererContainer.GetChild(i);
-                if (child.TryGetComponent<Renderer>(out Renderer renderer))
-                {
-                    renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
-                }
+                renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
             }
         }
     }
 
-    void Update()
+    private void LateUpdate()
     {
+        if (!IsOwner) return;
+
         positionGun();
 
-        if (c1 != null && IsValidVector3(c1.transform.position) && IsValidQuaternion(c1.transform.rotation)) {
-            casingPos = c1.transform.localPosition;
-            if (casingPos != casingPosPrev)
+        if (c1 != null && IsValidVector3(c1.localPosition))
+        {
+            Vector3 pos = c1.localPosition;
+            bool enabled = c1Renderer.enabled;
+
+            if (!sentOnce || pos != casingPosPrev || enabled != casingEnabledPrev)
             {
-                syncCasingServer(c1.transform.localPosition, c1.GetComponent<MeshRenderer>().enabled);
+                casingPosPrev = pos;
+                casingEnabledPrev = enabled;
+                syncCasingServer(pos, enabled);
             }
-            casingPosPrev = casingPos;
         }
 
-        if (gm1 != null && IsValidVector3(gm1.transform.position) && IsValidQuaternion(gm1.transform.rotation)) {
-            magPos = gm1.transform.localPosition;
-            if (magPos != magPosPrev)
+        if (gm1 != null && IsValidVector3(gm1.localPosition))
+        {
+            Vector3 pos = gm1.localPosition;
+            bool enabled = gm1Renderer.enabled;
+
+            if (!sentOnce || pos != magPosPrev || enabled != magEnabledPrev)
             {
-                syncMagServer(gm1.transform.localPosition, gm1.GetComponent<MeshRenderer>().enabled);
+                magPosPrev = pos;
+                magEnabledPrev = enabled;
+                syncMagServer(pos, enabled);
             }
-            magPosPrev = magPos;
         }
+
+        sentOnce = true;
     }
-    
+
     private void positionGun()
     {
-        if (g1 != null && IsValidVector3(g1.transform.position) && IsValidQuaternion(g1.transform.rotation))
-            gun.transform.position = g1.transform.position - new Vector3(0, 0.35f, 0);
-            gun.transform.rotation = g1.transform.rotation;
+        if (g1 == null || gun == null) return;
+        if (!IsValidVector3(g1.position) || !IsValidQuaternion(g1.rotation)) return;
+
+        gun.SetPositionAndRotation(g1.position - new Vector3(0f, 0.35f, 0f), g1.rotation);
     }
 
-    [ServerRpc]
-    private void syncCasingServer(Vector3 pos, bool enabled)
+    [ServerRpc(RunLocally = true)]
+    private void syncCasingServer(Vector3 pos, bool enabled, Channel channel = Channel.Unreliable)
     {
-        syncCasing(pos, enabled);
+        if (IsServerInitialized)
+            syncCasing(pos, enabled);
+        else
+            applyCasing(pos, enabled);   // owner's RunLocally pass
     }
 
-    [ObserversRpc]
-    private void syncCasing(Vector3 pos, bool enabled)
+    [ObserversRpc(ExcludeOwner = true)]
+    private void syncCasing(Vector3 pos, bool enabled, Channel channel = Channel.Unreliable)
+        => applyCasing(pos, enabled);
+
+    private void applyCasing(Vector3 pos, bool enabled)
     {
+        if (!IsValidVector3(pos)) return;
+
         casing.localPosition = pos;
-        casing.GetComponent<MeshRenderer>().enabled = enabled;
+        casingRenderer.enabled = enabled;
     }
 
-    [ServerRpc]
-    private void syncMagServer(Vector3 pos, bool enabled)
+    [ServerRpc(RunLocally = true)]
+    private void syncMagServer(Vector3 pos, bool enabled, Channel channel = Channel.Unreliable)
     {
-        syncMag(pos, enabled);
+        if (IsServerInitialized)
+            syncMag(pos, enabled);
+        else
+            applyMag(pos, enabled);
     }
 
-    [ObserversRpc]
-    private void syncMag(Vector3 pos, bool enabled)
+    [ObserversRpc(ExcludeOwner = true)]
+    private void syncMag(Vector3 pos, bool enabled, Channel channel = Channel.Unreliable)
+        => applyMag(pos, enabled);
+
+    private void applyMag(Vector3 pos, bool enabled)
     {
+        if (!IsValidVector3(pos)) return;
+
         gunMag.localPosition = pos;
-        gunMag.GetComponent<MeshRenderer>().enabled = enabled;
+        magRenderer.enabled = enabled;
     }
 
     private bool IsValidVector3(Vector3 vector)
